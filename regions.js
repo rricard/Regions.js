78dfcaf5eb12692e07c2af202850d41728ccaf61
Robin Ricard <ricard.robin@gmail.com>
2011-06-13 12:16:49 +0200

    Reorganisation + faster node treatment.

--- regions.js
+++ regions.js
@@ -1,114 +1,150 @@
 window.CSSRegions = (function( window, document, undefined ) {
   
+  /**
+   * Version
+   */
+  
   var version = "0.1",
   
-  CSSRegions = {},
-  
   /**
-   * Just put enough elements (that fits) into the region
-   * Returns the overflow in a container
+   * The main object, automation and public manipulation API
    */
   
-  integrate = function (elementsContainer, region) {
-    // This avoids the original copy's alteration 
-    elementsContainer = elementsContainer.clone();
+  CSSRegions = {
     
-    // This redefines the offsetParent for children
-    if (region.css("position") == "static")
-      region.css("position", "relative");
+    /**
+     * Stores the flows to be analyzed
+     */
     
-    // This introduces the elements
-    region.empty();
-    region.append(elementsContainer.children());
+    sets: [],
     
-    // Clears the top margin
-    region.children().first().css("margin-top", "0");
+    /**
+     * Automated CSS analyse and modifications listening
+     */
     
-    // This removes the overflow and return it
-    var ret = overflowRemover(region);
+    automation: function() {
+      CSSRegions.build($("#article"), [$("#article-region-1"), $("#article-region-2"), $("#article-region-3")]);
+    },
     
-    // Clears the bottom margin
-    region.children().last().css("margin-bottom", "0");
+    /**
+     * CSS Analyse to catch sets
+     */
     
-    return ret;
-  },
-  
-  /**
-   * Removes and return the overflow in a region
-   * for each node you want to analyze.
-   */
+    cssAnalyse: function(dom) {
+      
+    },
     
-  overflowRemover = function (elementsContainer, region, letteringForbidden) {
-    if (region === undefined) region = elementsContainer;
+    /**
+     * Introduce a flow into several regions (once)
+     * Returns the overflow in a DOM element
+     */
     
-    // This create an empty and hidden DOM node
-    var overflowContainer = $("<div></div>"),
-        elements = elementsContainer.children(),
-    // This stores the first element that had to be rejected
-        firstRejected = false;
-    
-    elements.each(function (index, element) {
-      element = $(element); // jQuery manipulable
-      if (
-        element.position().left + element.outerWidth(true) > region.width() ||
-        element.position().top + element.outerHeight(true) > region.height()
-      ) { // If it does not fit in the region :
-        element.detach();
-        if (firstRejected) { // And if a rejected element have been keeped.
-          // It's added directly into the overflow node
-          overflowContainer.append($("<span> </span>")); // Fix whitespace issues
-          overflowContainer.append(element);
-        } else {
-          // Or we keep it for later
-          firstRejected = element;
-        }
-      }
-    });
-    
-    if (firstRejected) { // The first rejected is taken
-      // If it's a text, treat it with Lettering.js
-      if (firstRejected.children().length === 0 && !letteringForbidden) {
-        firstRejected.lettering("words");
-        letteringForbidden = true;
-      }
-      // If he is not empty
-      if (firstRejected.children().length > 0) {
-        // He is reintroduced into the DOM
-        elementsContainer.append(firstRejected);
-        // The same treatment will be performed on him (recursion)
-        var removed = overflowRemover(firstRejected, region, letteringForbidden),
-        // His overflow will be moved into his parent's overflow
-            reintroduce = firstRejected.clone();
-        reintroduce.empty();
-        reintroduce.append(removed.children());
-        overflowContainer.prepend(reintroduce);
+    build: function (flowContainer, regionsArray) {
+      flowContainer.css("display", "none");
+      var overflow = RegionBuilder.integrate(flowContainer, regionsArray[0]);
+      regionsArray.shift();
+      if (regionsArray.length > 0) {
+        return CSSRegions.build(overflow, regionsArray);
       } else {
-        overflowContainer.prepend(firstRejected);
+        return overflow;
       }
     }
     
-    return overflowContainer;
-  };
+  },
   
   /**
-   * Introduce a flow into several regions (once)
-   * Returns the overflow in a DOM element
+   * The display and calculation core (private)
    */
   
-  CSSRegions.build = function (flowContainer, regionsArray) {
-    flowContainer.css("display", "none");
-    var overflow = integrate(flowContainer, regionsArray[0]);
-    regionsArray.shift();
-    if (regionsArray.length > 0) {
-      return CSSRegions.build(overflow, regionsArray);
-    } else {
-      return overflow;
+  RegionBuilder = {
+    
+    /**
+     * Just put enough elements (that fits) into the region
+     * Returns the overflow in a container
+     */
+    
+    integrate: function (elementsContainer, region) {
+      // This avoids the original copy's alteration 
+      elementsContainer = elementsContainer.clone();
+      
+      // This redefines the offsetParent for children
+      if (region.css("position") == "static")
+        region.css("position", "relative");
+      
+      // This introduces the elements
+      region.empty();
+      region.append(elementsContainer.children());
+      
+      // Clears the top margin
+      region.children().first().css("margin-top", "0");
+      
+      // This removes the overflow and return it
+      var ret = RegionBuilder.overflowRemover(region);
+      
+      // Clears the bottom margin
+      region.children().last().css("margin-bottom", "0");
+      
+      return ret;
+    },
+    
+    /**
+     * Removes and return the overflow in a region
+     * for each container you want to analyze.
+     */
+      
+    overflowRemover: function (elementsContainer, region, letteringForbidden) {
+      if (region === undefined) region = elementsContainer;
+      
+      // This create an empty and hidden DOM node
+      var overflowContainer = $("<div></div>"),
+          elements = elementsContainer.children(),
+      // This stores the first element that had to be rejected
+          firstRejected = false;
+      
+      elements.each(function (index, element) {
+        element = $(element); // jQuery manipulable
+        if (firstRejected) { // If a rejected element have been keeped.
+            // It's added directly into the overflow node
+            element.detach();
+            overflowContainer.append($("<span> </span>")); // Fix whitespace issues
+            overflowContainer.append(element);
+        } else if ( // If it does not fit in the region
+          element.position().top + element.outerHeight(true) >= region.height()
+        ) { 
+          element.detach();
+          firstRejected = element;
+        }
+      });
+      
+      if (firstRejected) { // The first rejected is taken
+        // If it's a text, treat it with Lettering.js
+        if (firstRejected.children().length === 0 && !letteringForbidden) {
+          firstRejected.lettering("words");
+          letteringForbidden = true;
+        }
+        // If he is not empty
+        if (firstRejected.children().length > 0) {
+          // He is reintroduced into the DOM
+          elementsContainer.append(firstRejected);
+          // The same treatment will be performed on him (recursion)
+          var removed = RegionBuilder.overflowRemover(firstRejected, region, letteringForbidden),
+          // His overflow will be moved into his parent's overflow
+              reintroduce = firstRejected.clone();
+          reintroduce.empty();
+          reintroduce.append(removed.children());
+          overflowContainer.prepend(reintroduce);
+        } else {
+          overflowContainer.prepend(firstRejected);
+        }
+      }
+      
+      return overflowContainer;
     }
   };
   
   $(function(){
-    
-    CSSRegions.build($("#article"), [$("#article-region-1"), $("#article-region-2"), $("#article-region-3")]);
+    // Let's start the automation
+    CSSRegions.automation();
   });
   
   return CSSRegions;
